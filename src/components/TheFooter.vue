29819d3eb45803a3a861dee4054efeddbd21cfcc
ider <326737833@qq.com>
2024-01-18 15:28:45 +0800

    enable dark switch buttom

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,7 +1,7 @@
 <template>
   <nav mt-6 inline-flex gap-2 text-xl>
-    <!-- <button icon-btn @click="toggleDark()">
+    <button icon-btn @click="toggleDark()">
       <div i-carbon-sun dark:i-carbon-moon />
-    </button> -->
+    </button>
   </nav>
 </template>
