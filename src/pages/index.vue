f8a08d3713f8b2cd188c8699d4c0a29cee99dfd3
ider <326737833@qq.com>
2024-01-18 15:23:42 +0800

    modify description

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -9,7 +9,7 @@ defineOptions({
     方法论
   </div>
   <div font-size-4 font-100>
-    取6个不同职业群体的所有page的linksin的集合 和 6个不同艺术的所有page的linksin集合, 计算两个集合的 google 距离。 <br>
+    取 10 个不同职业群体的所有page的linksin的集合 和 6个不同艺术的所有page的linksin集合, 计算两个集合的 google 距离。 <br>
     艺术从对应categetory出发，向下找2层，取这些category的所有page <br>
     职业群体从对应categetory出发，通过关键词过滤保留所有关于人的page <br>
     这个距离能反映职业群体和艺术的相关性。 <br>
